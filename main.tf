eb43a601e219944e40198718aff5f4fe14da9bc3
Taylor Owen <as0bu@users.noreply.github.com>
2020-04-02 10:00:29 -0600

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+variable "app_environment" {}
+
+data "terraform_remote_state" "tenant_workspace" {
+  backend = "remote"
+
+  config = {
+    organization = "KPSC"
+    workspaces = {
+      name = "openstack-tenants-${var.app_environment}"
+    }
+  }
+}
+
+module "mylabapp" {
+  # source = "../../git/terraform-openstack-app"
+  source  = "app.terraform.io/KPSC/app/openstack"
+  version = "0.0.2"
+
+  tenant_name       = data.terraform_remote_state.tenant_workspace.outputs.tenant_name
+  app_name          = "web"
+  app_environment   = var.app_environment
+  app_network       = data.terraform_remote_state.tenant_workspace.outputs.network_name
+  ext_net_name      = "public"
+  number_of_servers = 2
+  subnet_resource   = data.terraform_remote_state.tenant_workspace.outputs.subnet_resource
+}
